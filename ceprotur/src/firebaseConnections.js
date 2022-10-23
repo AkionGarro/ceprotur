4a92b93772a44812280d44b09de2be37bf5324c9
AkionGarro <carloscamp1008@estudianTEC.cr>
2022-10-23 14:33:18 -0600

    firebase configuration init

---
+++ ceprotur/src/firebaseConnections.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import "firebase/compat/firestore";
+export const conn = firebase.initializeApp({
+  projectId: "ceprotur",
+  appId: "1:415563712741:web:e611e5f1726622407aac0c",
+  storageBucket: "ceprotur.appspot.com",
+  locationId: "us-central",
+  apiKey: "AIzaSyC66ZPEzBBOAZw9EShjyS6QeBetBav9PWo",
+  authDomain: "ceprotur.firebaseapp.com",
+  messagingSenderId: "415563712741",
+});
+
+/*
+App information:
+  - App ID: 1:415563712741:web:e611e5f1726622407aac0c
+  - Display name: ceproturApp
+
+You can run this command to print out your new app's Google Services config:
+  firebase apps:sdkconfig WEB 1:415563712741:web:e611e5f1726622407aac0c
+*/
