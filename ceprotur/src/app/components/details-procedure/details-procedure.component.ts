bbbfdde9cfbfc2bdd7ca4ecec44692d8edfcbab1
AkionGarro <carloscamp1008@estudianTEC.cr>
2022-11-22 00:42:36 -0600

    Update file system

--- ceprotur/src/app/components/details-procedure/details-procedure.component.ts
+++ ceprotur/src/app/components/details-procedure/details-procedure.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RegisterService } from 'src/app/services/register.service';
-
+import {
+  ref,
+  Storage,
+  uploadBytes,
+  getDownloadURL,
+} from '@angular/fire/storage';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-details-procedure',
   templateUrl: './details-procedure.component.html',
@@ -10,9 +16,12 @@ import { RegisterService } from 'src/app/services/register.service';
 export class DetailsProcedureComponent implements OnInit {
   id: any;
   currentProcedure: any;
+  file: any;
+  fileRef: any;
   constructor(
     private route: ActivatedRoute,
     public service: RegisterService,
+    private storage: Storage,
     private router: Router
   ) {}
 
@@ -23,8 +32,32 @@ export class DetailsProcedureComponent implements OnInit {
       formData.append('idProcedure', this.id);
       this.service.getProcedureById(formData).subscribe((res) => {
         this.currentProcedure = res;
-        console.log(this.currentProcedure);
       });
     });
+  }
+
+  uploadFile($event: any) {
+    this.file = $event.target.files[0];
+  }
+
+  sendToFire() {
+    if (this.file != null) {
+      this.fileRef = ref(
+        this.storage,
+        'services/' +
+          localStorage['currentServiceId'].slice(1, -1) +
+          '/' +
+          localStorage['currentPhase'] +
+          '/' +
+          this.id +
+          '/' +
+          this.file.name
+      );
+      uploadBytes(this.fileRef, this.file).then((snapshot) => {
+        console.log(snapshot);
+        Swal.fire({ icon: 'success', title: 'Archivo subido' });
+        this.router.navigate(['/created-services']);
+      });
+    }
   }
 }
